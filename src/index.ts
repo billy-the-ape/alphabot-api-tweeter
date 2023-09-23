e0acdd98ff0eff28d64b33330f790229ace4c785
Billy <cryptobilly37@gmail.com>
2023-09-23 11:58:47 -0600

    Increase json parsing limit

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { sendRaffleTweet } from './tweet.js';
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 const PORT = process.env.PORT || 8080;
 
